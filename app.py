9af8df6a702b00dcd8e9fd6e2e4f5bf3dd914d1a
JHsu <hsu187@purdue.edu>
2023-01-21 11:36:16 -0500

    added base.html
    use this as template

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+#App routes
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
